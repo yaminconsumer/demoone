7f8f6b1e99e3b95d1d600e13a8625e2a0442b4f5
CSDevelopment52 <yamin@example.com>
2023-04-17 15:39:14 +0530

    asd

---
+++ src/DemoServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+namespace Yaminconsumer\Demo;
+use Illuminate\Support\ServiceProvider;
+
+class DemoServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+    }
+    public function register()
+    {
+        # code...
+    }
+}
+?>
---
+++ src/routes/web.php
@@ -0,0 +1,5 @@
+<?php 
+Route::get('demoy',function(){
+    return 'demoy';
+});
+?>
